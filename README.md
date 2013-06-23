a78845cca21310914b55634cc2c47a14cf35f410
Shane Rogers <rogerssh@tcd.ie>
2013-06-23 17:54:33 -0600

    rails app intialized with README edited

---
+++ README.md
@@ -0,0 +1,3 @@
+# Railsplate
+
+Template rails app for easy project start. Configured with rspec & capybara for testing, PostgreSQL for database and a more fleshed out Gemfile.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsplate::Application.config.secret_token = '0e694fadb647287dfe8b609a7a6d75311ecdccf1c0a6fef55aead97e781e607e9941b9651580368411eb89ab21252ac21f6b13147d135b7e7b6c0c6f59bd22ca'
