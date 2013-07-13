1e33f9a91e6b46615dda41a1f065586013d8f1e1
Shane Rogers <rogerssh@tcd.ie>
2013-07-12 19:03:26 -0600

    session setting resolved

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  # def current_user
+  # 	if Doctor.find(session[:doctor_id])
+  # 		doctor
+  # 	else
+  # 		Patient.find(session[:patient_id])
+  # 	end
+  # end
+
+  def doctor(params)
+    Doctor.where(email: params[:sessions][:email]).first
+  end
+
+  def patient(params)
+    Patient.where(email: params[:sessions][:email]).first
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,22 +5,15 @@ class SessionsController < ApplicationController
   def create
     doctor = doctor(params)
     patient = patient(params)
-    binding.pry
     if doctor
+      session[:doctor_id] = doctor.id
       redirect_to doctor_path(doctor) and return
     elsif patient
+      session[:patient_id] = patient.id
       redirect_to patient_path(patient) and return
     else
       redirect_to root_path, notice: "Log-in un-successful please retry"
     end
-  end
-
-  def doctor(params)
-    Doctor.where(email: params[:sessions][:email]).first
-  end
-
-  def patient(params)
-    Patient.where(email: params[:sessions][:email]).first
   end
 
   def destroy
