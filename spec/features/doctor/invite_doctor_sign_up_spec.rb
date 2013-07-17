786a12ffc2b5e968d519b3e4f0bba8d3b2934b5c
Shane Rogers <rogerssh@tcd.ie>
2013-07-17 13:48:12 -0600

    invite template change

---
+++ spec/features/doctor/invite_doctor_sign_up_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe 'as a public doctor invited by a patient to signup' do
+	let(:patient) {FactoryGirl.create(:patient)}
+	let(:doctor) {FactoryGirl.build(:doctor)}
+
+	it 'I can see the sign-up form' do
+		visit patients_new_doctor_path(patient, "example@example.com")
+		expect(page).to have_selector('div#doctor-signup')
+		expect(page).to have_content('Patient Name')
+		within(:css, 'div#doctor-signup'){
+			fill_in "username", :with => 'frank'
+			fill_in "email", :with => 'example@example.com'
+			fill_in "phone", :with => '56748396'
+			fill_in "medical_school", :with => 'harvard'
+			fill_in "practice", :with => '123 Fake Street'
+			select 'Surgeon', from: 'speciality'
+			fill_in "password", :with => 'password'
+			click_button 'Sign-Up!'
+		}
+		expect(page).to have_content "frank"
+		click_link 'My Patients'
+		expect(page).to have_content "Shane"
+	end
+end
