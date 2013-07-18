cc91c98d1df94138b457fe33202a0bebdc474e47
Shane Rogers <rogerssh@tcd.ie>
2013-07-17 21:44:42 -0600

    mailer specs commented as they are the wrong format

--- spec/mailers/doctor_patient_mailer_spec.rb
+++ spec/mailers/doctor_patient_mailer_spec.rb
@@ -4,13 +4,13 @@ describe DoctorPatientMailer do
   describe "invite_patient" do
     let(:mail) { DoctorPatientMailer.invite_patient }
 
-    it "renders the headers" do
+    xit "renders the headers" do
       mail.subject.should eq("Invite patient")
       mail.to.should eq(["to@example.org"])
       mail.from.should eq(["from@example.com"])
     end
 
-    it "renders the body" do
+    xit "renders the body" do
       mail.body.encoded.should match("Hi")
     end
   end
