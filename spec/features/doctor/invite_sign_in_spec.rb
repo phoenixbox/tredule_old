96db7ba9ddd62ce88b5624aa114982e01f13017c
Shane Rogers <rogerssh@tcd.ie>
2013-07-17 21:59:44 -0600

    prep for doctor sign-in specs

---
+++ spec/features/doctor/invite_sign_in_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe 'I am a member doctor' do
+	it 'I can sign in through the email invite' do
+		# visit the doctor sign-in and create association path
+		# fill in the form fields
+		# expect page to be the profile page
+		# click the my patients link
+		# see the associated patient in the list
+	end
+end
